abfc5517cbbd14f859a2030a3773cccfa610f17d
antonyrajesh123 <arockia-antony-rajesh.maria-antony@hpe.com>
2024-11-21 22:10:04 +0530

    CHANGED THE NAME OF DNN

--- test_deployment.py
+++ test_deployment.py
@@ -1,19 +1,19 @@
 import torch
 from torchvision import datasets, transforms
-from train import DNN
+from train import SmallDNN
 
 def test_deployment():
     # Load the deployed model (using the most recent model saved with timestamp)
     model_files = sorted(glob.glob("model_*.pth"), reverse=True)  # Sort models by date
     latest_model = model_files[0]  # Get the most recent model
 
-    model = DNN()
+    model = SmallDNN()
     model.load_state_dict(torch.load(latest_model))
     model.eval()
 
     # Run inference on a sample image from MNIST test set
     transform = transforms.Compose([transforms.ToTensor()])
-    test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=False)
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=False )
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True)
 
     # Pick a random image for testing
